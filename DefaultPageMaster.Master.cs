1da013d248b2277c615b1afda6885fc013683e9c
tuankiet04 <130892029+ttkiet21@users.noreply.github.com>
2024-09-27 16:28:13 +0700

    init commit

---
+++ DefaultPageMaster.Master.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace de1
+{
+    public partial class DefaultPageMaster : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadCourses();
+
+            }
+
+        }
+        private void LoadCourses()
+        {
+            QLKhoaHocEntities1 db = new QLKhoaHocEntities1();
+            try
+            {
+                var courses = db.Categories
+                       .Select(cat => new
+                       {
+                           CatName = cat.CatName,
+                           Count = cat.Courses.Count()
+                       })
+                       .ToList();
+
+                dlCourses.DataSource = courses;
+                dlCourses.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+        }
+        protected void btnCategory_Click(object sender, EventArgs e)
+        {
+            LinkButton btn = (LinkButton)sender;
+            string categoryName = btn.CommandArgument;
+
+            LoadCourse(categoryName);
+
+        }
+        protected void detailClick(object sender, EventArgs e)
+        {
+            LinkButton btn = (LinkButton)sender;
+            string id = btn.CommandArgument;
+            Session["Id"] = id;
+
+        }
+        private void LoadCourse(string name)
+        {
+            QLKhoaHocEntities1 db = new QLKhoaHocEntities1();
+            try
+            {
+                var course = db.Courses.Where(c => c.Category.CatName == name).ToList();
+                rptCourse.DataSource = course;
+                rptCourse.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+
+    }
+}
